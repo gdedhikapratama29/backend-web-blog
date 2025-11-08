48ab35ca8d5a3982bc3a3f0ee52bf3ac5e115004
gdedhikapratama29 <gdedhikapratama29@gmail.com>
2025-11-08 22:44:33 +0800

    chore: server is running

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,133 @@
+import { Request, Response } from "express";
+import  * as Yup from 'yup';
+
+import UserModel from "../models/user.model";
+import { encrypt } from "../utils/encryption";
+import { generateToken } from "../utils/jwt";
+import { IRequest } from "../middlewares/auth.middleware";
+
+type TRegister = {
+    fullName: string,
+    username: string,
+    email: string,
+    password: string,
+    confirmPassword: string
+};
+
+type Tlogin = {
+    identifier: string;
+    password: string;
+}
+
+const registerValidateSchema = Yup.object({
+    fullName: Yup.string().required(),
+    username: Yup.string().required(),
+    email: Yup.string().email().required(),
+    password: Yup.string().required(),
+    confirmPassword: Yup.string().required().oneOf([Yup.ref('password'), ""], "passwords not match")
+})
+
+export default {
+   async register( req: Request, res: Response) {
+    const { fullName, username, email, password, confirmPassword
+    } = req.body as unknown as TRegister;
+
+     try {
+        await registerValidateSchema.validate({
+        fullName,
+        username,
+        email,
+        password,
+        confirmPassword
+    });
+
+    const result = await UserModel.create({
+        fullName,
+        username,
+        email,
+        password
+    })
+    
+    res.status(200).json({
+        message: 'success register',
+        data: result,
+    })
+     } catch (error) {
+        const err = error as unknown as Error;
+        res.status(400).json({
+            message: err.message,
+            data: null,
+        })
+     }
+   },
+
+   async login(req: Request, res: Response) {
+    const {
+        identifier,
+        password
+    } = req.body as unknown as Tlogin;
+     try {
+       const userByIdentifier = await UserModel.findOne({
+           $or: [
+               {
+                email: identifier
+               },
+               {
+                username: identifier
+               },
+           ],
+       });
+
+       if (!userByIdentifier) {
+        return res.status(404).json({
+            message: "user not found",
+            data: null,
+        })
+       }
+
+       //validasi password
+       const validatePassword: boolean = encrypt(password) === userByIdentifier.password;
+
+       if (!validatePassword) {
+         return res.status(404).json({
+            message: "user not found",
+            data: null,
+        });
+       } 
+
+       const token =  generateToken({
+        id: userByIdentifier._id,
+        role: userByIdentifier.role,
+       }) 
+
+       res.status(200).json({
+        message: "login success",
+        data: token,
+       })
+     } catch (error) {
+        const err = error as unknown as Error;
+        res.status(400).json({
+            message: err.message,
+            data: null,
+        })
+     };
+   },
+
+   async me(req: IRequest, res: Response) {
+    try {
+      const user = req.user;
+      const result = await UserModel.findById(user?.id);
+
+      res.status(200).json({
+        message: "success get user profile",
+        data: result,
+      })
+    } catch (error) {
+        const err = error as unknown as Error;
+        res.status(400).json({
+            message: err.message,
+            data: null,
+        }) 
+    }
+   },
+};
---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import router from "./routes/api"
+import bodyParser from "body-parser";
+import db from './utils/database';
+
+async function init () {
+    try {
+
+    const result = await db();
+
+    console.log("database status: ", result)
+
+
+    const app = express();
+    const PORT = 3000;
+
+      app.get('/', (req, res) => {
+        res.status(200).json({
+            message: "server is running",
+            data: null
+        })
+    })
+
+    app.use(bodyParser.json())
+    app.use('/api', router)
+
+  
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    }); 
+    } catch (error) {
+       console.log(error)
+    }
+}
+
+init();
